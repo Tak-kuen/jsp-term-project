cfd12a28e1b7dfbeb2962d935daf400bf2d65682
Tak-kuen <songtak456@naver.com>
2018-12-04 18:15:10 +0900

    12/4

--- jsp_project/WebContent/mngr/managerMain.js
+++ jsp_project/WebContent/mngr/managerMain.js
@@ -6,17 +6,4 @@ var status=true;
 
 $(document).ready(function() {
 	
-	
-	$("#menu").click(function() {
-		location.href="/jsp_project/mg/cafe/menuMain.do";
-	});
-	$("#updateProduct").click(function() {
-		location.href="/jsp_project/mg/booklist.do?book_kind=all";
-	});
-	$("#orderedProduct").click(function() {
-		location.href="/jsp_project/mg/orderList.do";
-	});
-	$("#qna").click(function() {
-		location.href="/jsp_project/mg/managerMain.do";
-	});
 });
--- jsp_project/src/coffee/command/MileFormAction.java
+++ jsp_project/src/coffee/command/MileFormAction.java
@@ -3,10 +3,11 @@ package coffee.command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ManagerLoginFormAction implements CommandAction {
+public class MileFormAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
-		return "/mngr/logon/mLoginForm.jsp";
+		return "/mngr/mileage/mileageForm.jsp";
 	}
+
 }
